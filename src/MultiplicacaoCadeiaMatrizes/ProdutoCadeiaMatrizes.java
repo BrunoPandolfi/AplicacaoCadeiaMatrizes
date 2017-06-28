a615a7184be321748b0930e9e7256cbe8c14f643
Akiro <Akiro@192.168.0.105>
2017-06-27 21:56:25 -0300

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/MultiplicacaoCadeiaMatrizes/ProdutoCadeiaMatrizes.java
+++ src/MultiplicacaoCadeiaMatrizes/ProdutoCadeiaMatrizes.java
@@ -44,6 +44,34 @@ public class ProdutoCadeiaMatrizes
             }
         }
     }
+    
+    
+    public int matrix_Chain_Order_Rec(int p[], int i, int j)
+    {
+        
+        if (i == j)
+            return 0;
+ 
+        int min = Integer.MAX_VALUE;
+ 
+        // place parenthesis at different places between first
+        // and last matrix, recursively calculate count of
+        // multiplications for each parenthesis placement and
+        // return the minimum count
+        for (int k=i; k<j; k++)
+        {
+            int count = matrix_Chain_Order_Rec(p, i, k) +
+                        matrix_Chain_Order_Rec(p, k+1, j) +
+                        p[i-1]*p[k]*p[j];
+ 
+            if (count < min)
+                min = count;
+        }
+ 
+        // Return minimum count
+        return min;
+    }
+
 
     public String print_optimal_parens(int i, int j)
     {
